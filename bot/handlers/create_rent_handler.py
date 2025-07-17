535829d38172998a59fc60ceb92b8e5081fa9f52
muhammadnur <muhammadnurpdp@gmail.com>
2025-07-17 18:22:24 +0500

    First Commit

--- bot/handlers/create_rent_handler.py
+++ bot/handlers/create_rent_handler.py
@@ -56,7 +56,7 @@ async def get_deadline(msg: types.Message, state: FSMContext):
             await msg.answer("⚠️ Дата не должна быть в прошлом или совпадать с сегодняшним днем.")
             return
 
-        price_per_day = int(data["amount"])
+        price_per_day = int(data["amount"] / remaining_days)
 
         formatted_deadline = deadline_date.strftime("%Y-%m-%d")
 

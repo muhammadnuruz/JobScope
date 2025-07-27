61ceeb909f7595f18781b51421a7e38a6972f33a
muhammadnur <muhammadnurpdp@gmail.com>
2025-07-27 14:20:26 +0500

    xss

--- bot/functions/main.py
+++ bot/functions/main.py
@@ -28,7 +28,7 @@ async def on_startup(dp: Dispatcher):
 
     scheduler.add_job(
         func=lambda: asyncio.run(run_get_advert()),
-        trigger=CronTrigger(hour=14, minute=20, timezone=timezone("Asia/Tashkent")),
+        trigger=CronTrigger(hour=14, minute=23, timezone=timezone("Asia/Tashkent")),
     )
 
     scheduler.add_job(

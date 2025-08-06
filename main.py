6b36088d391500202d5dbe3a521f1ad9188cf1b0
muhammadnur <muhammadnurpdp@gmail.com>
2025-08-06 21:07:08 +0500

    Update embedding function
    if packQuantity > 0, it will be embedding

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from bot import handlers
 
 admins = [1974800905, 999090234]
 
+# Loggerlar darajasini warning yoki yuqoriga tushirish
+logging.basicConfig(level=logging.WARNING)
+logging.getLogger("aiogram").setLevel(logging.WARNING)
+logging.getLogger("aiohttp").setLevel(logging.WARNING)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.WARNING)
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
